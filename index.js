86fc0d0c46f33417557e25cf1fac96e83e8175ca
Stephen <skaranja654@gmail.com>
2023-03-22 13:20:33 +0300

    Still attempting

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, item) {
+  const result = drivers.filter(function(driver){
+    return driver === item;
+  });
+  return result;
+}
+
+console.log(findMatching(drivers, "Bobby"));
+
+function fuzzyMatch(driversArray) {
+  return driversArray.filter(function(driver) {
+    return driver.startsWith('Sa');
+  });
+}
+
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ];
+function matchName(driversArray, searchTerm) {
+    return driversArray.filter(function(driver) {
+      return driver.name === searchTerm;
+    });
+  }
+
